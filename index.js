69d62bfb207cb44ca00e9f32610a4d094a476bca
dimabytes <dimabytes@gmail.com>
2021-02-07 15:54:22 +0300

    change to index.html

--- index.js
+++
@@ -1,4 +0,0 @@
-import init from './src/init';
-import 'bootstrap';
-
-export default init;
--- src/init.js
+++ src/init.js
@@ -19,6 +19,7 @@ export default () => {
     });
 
     const element = document.getElementById('app');
+    console.log(element);
     const app = createApp(element);
     app.setControllers();
     app.getNewPosts();
