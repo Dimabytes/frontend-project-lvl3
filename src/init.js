848f416d603b9a4489400dcb97c71e7ec714311c
dimabytes <dimabytes@gmail.com>
2021-02-22 18:49:40 +0300

    debug

--- src/init.js
+++ src/init.js
@@ -1,24 +1,25 @@
 import 'bootstrap';
 import i18n from 'i18next';
-import { setLocale } from 'yup';
+import yup from 'yup';
 import createApp from './App.js';
 import resources from './locales';
 
-export default () => i18n.init({
-  lng: 'en',
-  debug: false,
-  resources,
-}).then(() => {
-  console.log(i18n.t('validationErrors.required'));
-  // setLocale({
-  //   mixed: {
-  //     required: i18n.t('validationErrors.required'),
-  //   },
-  //   string: {
-  //     url: i18n.t('validationErrors.url'),
-  //   },
-  // });
-  // const app = createApp();
-  // app.setControllers();
-  // app.getNewPosts();
-});
+export default () => {
+  i18n.init({
+    lng: 'en',
+    resources,
+  }).then(() => {
+    console.log(i18n.t('validationErrors.required'));
+    yup.setLocale({
+      mixed: {
+        required: i18n.t('validationErrors.required'),
+      },
+      string: {
+        url: i18n.t('validationErrors.url'),
+      },
+    });
+    const app = createApp();
+    app.setControllers();
+    app.getNewPosts();
+  });
+};
