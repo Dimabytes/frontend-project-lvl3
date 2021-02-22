349d6e738dd5526ad84e6df7c4f675773cb612ea
dimabytes <dimabytes@gmail.com>
2021-02-22 18:31:03 +0300

    debug

--- src/init.js
+++ src/init.js
@@ -4,21 +4,25 @@ import { setLocale } from 'yup';
 import createApp from './App.js';
 import resources from './locales';
 
-export default () => i18n.init({
-  lng: 'ru',
-  debug: false,
-  resources,
-}).then(() => {
-  console.log(i18n.t('validationErrors.required'));
-  setLocale({
-    mixed: {
-      required: i18n.t('validationErrors.required'),
-    },
-    string: {
-      url: i18n.t('validationErrors.url'),
-    },
+export default () => {
+  console.log('hello');
+  const a = i18n.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  }).then(() => {
+    console.log(i18n.t('validationErrors.required'));
+    setLocale({
+      mixed: {
+        required: i18n.t('validationErrors.required'),
+      },
+      string: {
+        url: i18n.t('validationErrors.url'),
+      },
+    });
+    const app = createApp();
+    app.setControllers();
+    app.getNewPosts();
   });
-  const app = createApp();
-  app.setControllers();
-  app.getNewPosts();
-});
+  return a;
+};
