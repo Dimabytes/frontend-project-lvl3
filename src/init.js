73783c5d297f2302b92c2fb140266b2a1e99897d
dimabytes <dimabytes@gmail.com>
2021-02-22 18:32:41 +0300

    debug

--- src/init.js
+++ src/init.js
@@ -1,28 +1,24 @@
 import 'bootstrap';
-import i18n from 'i18next';
+import i18n from 'i18next/dist/cjs/i18next.js';
 import { setLocale } from 'yup';
 import createApp from './App.js';
 import resources from './locales';
 
-export default () => {
-  console.log('hello');
-  const a = i18n.init({
-    lng: 'ru',
-    debug: false,
-    resources,
-  }).then(() => {
-    console.log(i18n.t('validationErrors.required'));
-    setLocale({
-      mixed: {
-        required: i18n.t('validationErrors.required'),
-      },
-      string: {
-        url: i18n.t('validationErrors.url'),
-      },
-    });
-    const app = createApp();
-    app.setControllers();
-    app.getNewPosts();
+export default () => i18n.init({
+  lng: 'ru',
+  debug: false,
+  resources,
+}).then(() => {
+  console.log(i18n.t('validationErrors.required'));
+  setLocale({
+    mixed: {
+      required: i18n.t('validationErrors.required'),
+    },
+    string: {
+      url: i18n.t('validationErrors.url'),
+    },
   });
-  return a;
-};
+  const app = createApp();
+  app.setControllers();
+  app.getNewPosts();
+});
