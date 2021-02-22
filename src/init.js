ba13387c6fb7a8279ad89bf97eeaadb9503e0c16
dimabytes <dimabytes@gmail.com>
2021-02-22 19:02:43 +0300

    debug

--- src/init.js
+++ src/init.js
@@ -1,21 +1,22 @@
 import 'bootstrap';
-import i18n from 'i18next';
+import i18next from 'i18next/dist/cjs/i18next.js';
 import * as yup from 'yup';
 import createApp from './App.js';
 import resources from './locales';
 
 export default () => {
-  i18n.init({
-    lng: 'en',
+  i18next.init({
+    lng: 'ru',
+    debug: false,
     resources,
   }).then(() => {
-    console.log(i18n.t('validationErrors.required'));
+    console.log(i18next.t('validationErrors.required'));
     yup.setLocale({
       mixed: {
-        required: i18n.t('validationErrors.required'),
+        required: i18next.t('validationErrors.required'),
       },
       string: {
-        url: i18n.t('validationErrors.url'),
+        url: i18next.t('validationErrors.url'),
       },
     });
     const app = createApp();
